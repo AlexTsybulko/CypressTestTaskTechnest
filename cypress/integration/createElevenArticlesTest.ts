0337878090bd02db0606953dcc651e8beae4bc89
atsybulko <atsybulko@elinext.net>
2021-08-19 23:09:01 +0300

    adding headless login and page objects

--- cypress/integration/createElevenArticlesTest.ts
+++ cypress/integration/createElevenArticlesTest.ts
@@ -1,5 +1,8 @@
+import {HomePage} from '../support/pageObjects/homePage.po';
+
 describe('Test task', () => {
     it('11 articles could be created', () => {
-        cy.visit('http://localhost:4200/#')
+        cy.login(Cypress.env("EMAIL"), Cypress.env("PASSWORD"));
+        const homePage = new HomePage();
     });
 });
